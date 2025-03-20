408b5b17d0e05410b705bd35b4c38bd0d98737bc
Felipe <felipeandr3@hotmail.com>
2025-03-20 17:24:10 -0300

    Nível Aventureiro v2.0

--- xadrez.c
+++ xadrez.c
@@ -1,42 +1,111 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int torre = 0, mov_Torre = 5;
-    int bispo = 0, mov_Bispo = 5;
-    int rainha = 0, mov_Rainha = 8;
+int main()
+{
+    // Variáveis de movimentação das peças do xadrez, além de variáveis para linha e coluna do tabuleiro
+    int movimento_cima, movimento_baixo, movimento_direita, movimento_esquerda, movimento_diagonal;
+    int linha, coluna;
 
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-    printf("Movimentação do Bispo:\n");
-    for (bispo; bispo < mov_Bispo; bispo++)
+    // Tabuleiro de Xadrez: https://i.imgur.com/CLxvjac.png
+    // O Bispo irá se mover da posição C1 para H6.
+    linha = 1; // iniciando na parte inferior do tabuleiro, linha 1
+    coluna = 3; // iniciando na coluna C do tabuleiro, terceira coluna da esquerda para direita
+
+    printf("\nMovimentação do Bispo:\n");
+    printf("Posição inicial do Bispo: (coluna: C, linha: 1)\n");
+
+    // Loop for para movimentar o Bispo na diagonal até (H6)
+    for (movimento_diagonal = 0; movimento_diagonal < 5; movimento_diagonal++)
     {
-        printf("Bispo se moveu para a diagonal: Cima - Direita\n");
+        linha++;  // Move uma linha para cima
+        coluna++; // Move uma coluna para a direita
+
+        printf("Bispo movendo na diagonal: (coluna: %c, linha: %d)\n", 'A' + (coluna - 1), linha);
+        for (int i = 0; i < 1000000000; i++)
+        {
+            // Apenas para simular um atraso
+        }
     }
 
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // Tabuleiro de Xadrez: https://i.imgur.com/CLxvjac.png
+    // A Torre irá se mover da posição B4 para G4.
+    linha = 4; // iniciando parte inferior do tabuleiro, linha 4
+    coluna = 2; // iniciando na coluna B do tabuleiro, segunda coluna da esquerda para direita
+
     printf("\nMovimentação da Torre:\n");
-    while (torre < mov_Torre)
+    printf("Posição inicial da Torre: (coluna: B, linha: 4)\n");
+
+    // Loop while para movimentar a Torre para direita até (G4)
+    while (coluna < 7)
     {
-        printf("Torre se moveu para a direita.\n");
-        torre++;
+        coluna++; // Move para a direita
+
+        printf("Torre movendo para a direita: (coluna: %c, linha: %d)\n", 'A' + (coluna - 1), linha);
+        for (int i = 0; i < 1000000000; i++)
+        {
+            // Apenas para simular um atraso
+        }
     }
 
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // Tabuleiro de Xadrez: https://i.imgur.com/CLxvjac.png
+    // A Rainha irá se mover da posição H3 para A3.
+    linha = 3; // iniciando na parte inferior do tabuleiro, linha 3
+    coluna = 8; // iniciando na coluna H do tabuleiro, primeira coluna da direita para esquerda
+
     printf("\nMovimentação da Rainha:\n");
+    printf("Posição inicial da Rainha: (coluna: H, linha: 3)\n");
+
+    // Loop do-while para movimentar a Rainha para esquerda até (A3)
     do
     {
-        printf("Rainha se moveu para a esquerda.\n");
-        rainha++;
+        coluna--; // Move para a esquerda
+
+        printf("Rainha movendo para a esquerda: (coluna: %c, linha: %d)\n", 'A' + (coluna - 1), linha);
+        for (int i = 0; i < 1000000000; i++)
+        {
+            // Apenas para simular um atraso
+        }
     }
-    while (rainha < mov_Rainha);
+    while (coluna > 1); // 1 corresponde à coluna A do tabuleiro
+
+    // Implementação de Movimentação do Cavalo
+    // Tabuleiro de Xadrez: https://i.imgur.com/CLxvjac.png
+    // O Cavalo irá se mover da posição B8 para A6.
+    linha = 8; // iniciando na parte superior do tabuleiro, linha 8
+    coluna = 2; // iniciando na coluna B do tabuleiro, segunda coluna da esquerda para direita
+
+    printf("\nMovimentação do Cavalo:\n");
+    printf("Posição inicial do Cavalo: (coluna: B, linha: 8)\n");
+
+    // Loop em for para movimentar o cavalo duas casas para baixo
+    for (movimento_baixo = 0; movimento_baixo < 2; movimento_baixo++)
+    {
+        linha--; // Movendo para baixo
+        printf("Cavalo movendo para baixo: (coluna: %c, linha: %d)\n", 'A' + (coluna - 1), linha);
+        // Loop aninhado para simular um atraso
+        for (int i = 0; i < 1000000000; i++)
+        {
+            // Apenas para simular um atraso
+        }
+    }
+
+    // Loop em while para movimentar o cavalo uma casa para a esquerda
+    // Variável de início
+    movimento_esquerda = 0;
+    while (movimento_esquerda < 1)
+    {
+        coluna--; // Movendo para a esquerda
+        printf("Cavalo movendo para a esquerda: (coluna: %c, linha: %d)\n", 'A' + (coluna - 1), linha);
+        // Loop aninhado para simular um atraso
+        for (int i = 0; i < 1000000000; i++)
+        {
+            // Apenas para simular um atraso
+        }
+        movimento_esquerda++;
+    }   
 
     return 0;
 }
